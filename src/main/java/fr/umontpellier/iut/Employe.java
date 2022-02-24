2237bd5ab97c7f020191c3c1c0684ad7c63e24e5
Clément Oiry <clement.oiry@etu.umontpellier.fr>
2022-02-24 08:53:05 +0100

    Création du builder employé

--- src/main/java/fr/umontpellier/iut/Employe.java
+++ src/main/java/fr/umontpellier/iut/Employe.java
@@ -15,4 +15,41 @@ public class Employe {
         this.nbHeures = nbHeures;
     }
 
+    public static class EmployeBuilder {
+        private String nrINSEE;
+        private String nom;
+        private int echelon;
+        private double base;
+        private double nbHeures;
+
+        public EmployeBuilder setNrINSEE(String nrINSEE) {
+            this.nrINSEE = nrINSEE;
+            return this;
+        }
+
+        public EmployeBuilder setNom(String nom) {
+            this.nom = nom;
+            return this;
+        }
+
+        public EmployeBuilder setEchelon(int echelon) {
+            this.echelon = echelon;
+            return this;
+        }
+
+        public EmployeBuilder setBase(double base) {
+            this.base = base;
+            return this;
+        }
+
+        public EmployeBuilder setNbHeures(double nbHeures) {
+            this.nbHeures = nbHeures;
+            return this;
+        }
+
+        public Employe createEmploye() {
+            return new Employe(nrINSEE, nom, echelon, base, nbHeures);
+        }
+    }
+
 }
